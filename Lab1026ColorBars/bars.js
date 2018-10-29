3f5737999d5686b56c9f72b0d639e7b04ef3fe26
sophiemorgan4 <43447326+sophiemorgan4@users.noreply.github.com>
2018-10-29 08:48:56 -0700

    Add files via upload

---
+++ Lab1026ColorBars/bars.js
@@ -0,0 +1,23 @@
+var bars = [];
+//Instance variables
+this.l = length
+this.w = width
+this.col = color(x,y,z)
+
+this.run = function(){
+}
+
+this.update = function(){
+
+  function myBubbleSort(){
+    var values =
+
+
+
+
+  }
+}
+
+this.render = function(){
+
+}
---
+++ Lab1026ColorBars/sketch.js
@@ -0,0 +1,15 @@
+
+//  This is a comment
+//  The setup function function is called once when your program begins
+function setup() {
+  var cnv = createCanvas(800, 800);
+  cnv.position((windowWidth-width)/2, 30);
+  background(5, 5, 5);
+  fill(200, 30, 150);
+  bar = new Bar(l, w, col)
+
+}
+
+//  The draw function is called @ 30 fps
+function draw() {
+}
