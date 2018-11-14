91f5f933b73e2409be9214ed7e19a97ea737bb57
sophiemorgan4 <43447326+sophiemorgan4@users.noreply.github.com>
2018-11-14 08:50:59 -0800

    Add files via upload

--- 1114SnakeGame/sketch.js
+++ 1114SnakeGame/sketch.js
@@ -1,17 +1,28 @@
-
+var rows, cols;
+var w = 20;
+var snake;
 //  This is a comment
 //  The setup function function is called once when your program begins
 function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
   background(5, 5, 5);
+  cols = width/w;
+  rows = height/w;
+  frameRate(1);
   fill(200, 30, 150);
-  new Snake = snake(createVector(),createVector())
-
-
+  snake = new Snake(createVector(width/2, height/2), createVector(1,0));
 }
 
 //  The draw function is called @ 30 fps
 function draw() {
+  snake.run();
+}
 
+function keyPressed(){
+  text(keyCode, 50, 50);
+
+  if(keyCode === UP_ARROW){
+    snake.vel = createVector(0,-1*w);
+  }
 }
--- 1114SnakeGame/snake.js
+++ 1114SnakeGame/snake.js
@@ -1,14 +1,18 @@
 //Snake Constructor function
 
 function Snake(loc,vel){
-
+  this.loc = loc;
+  this.vel = vel;
 
   this.run(){
     this.update();
     this.render();
   }
 
-  this.update();
+  this.update(){
+    this.vel.mult(w);
+    this.loc.add(this.vel)
+  }
 
   this.render();
 }
